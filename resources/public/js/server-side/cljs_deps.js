aaa5e4e60241fe7f4b1690c0514b1e995433973d
Chris Westra <cwestra@gmail.com>
2016-12-24 07:07:00 -0500

    Remove tmp files

--- resources/public/js/server-side/cljs_deps.js
+++
@@ -1,5 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.math.Integer', 'goog.string.StringBuffer', 'goog.array', 'goog.math.Long']);
-goog.addDependency("../B73F007.js", ['cljs.nodejs'], ['cljs.core']);
-goog.addDependency("../express_demo/server.js", ['express_demo.server'], ['cljs.core', 'cljs.nodejs']);
-goog.addDependency("../BCE03FE.js", ['cljs.nodejscli'], ['cljs.core', 'cljs.nodejs']);
